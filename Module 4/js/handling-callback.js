ef2353eaa94803a584a22c12b57c7dabca23aac4
Lujun Weng <loyweng@163.com>
2016-04-10 21:22:22 +0800

    Module 4 finished

---
+++ Module 4/js/handling-callback.js
@@ -0,0 +1,10 @@
+function handleClick(callback) {
+    alert('This button has been clicked!');
+    if (callback) {
+        callback();
+    }
+}
+
+function doMore() {
+    alert('I could process more logic here');
+}
